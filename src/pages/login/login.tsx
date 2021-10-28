fb31b9114bb506b8f07bab4354cd294f13a51af9
FZxiao233 <fzxiao@outlook.com>
2021-10-29 00:39:44 +0800

    feat(login): update login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -4,7 +4,7 @@ import { View, Text, Input, Button, Form } from "@tarojs/components";
 import { login } from "../../api/api";
 import { LoginProps } from "../../api/Iapi";
 
-export default function Login({setIsLogged}) {
+export default function Login() {
     const [token, setToken] = useState()
     useEffect(() => {
         if (token === undefined) return;
@@ -12,13 +12,18 @@ export default function Login({setIsLogged}) {
             key: "token",
             data: token
         }).then(() => {
-            setIsLogged(true)
+            Taro.switchTab({
+                url: "/page/index/index"
+            })
         })
-    }, [token, setIsLogged])
+    }, [token])
     const handleLogin = (e) => {
+        Taro.showLoading()
         login(e.detail.value as LoginProps)
             .then((res) => {
-                if (res.data.data !== null) {
+                Taro.hideLoading()
+
+                if (res.data.code === 0) {
                     setToken(res.data.data)
                 } else {
                     Taro.showToast({
@@ -30,12 +35,12 @@ export default function Login({setIsLogged}) {
             })
     }
     return (
-        <View className='container'>
+        <View className='container bg-cover' style={{backgroundImage: "url('../../static/login_bg.png')"}}>
             <View className='px-12 flex flex-col h-screen'>
                 <View className='flex flex-col justify-center h-full'>
                     <View className='flex flex-col'>
-                        <Text className='text-2xl'>登录</Text>
-                        <Text className='text-gray-300'>绑定信息门户</Text>
+                        <Text className='text-3xl font-bold'>登录</Text>
+                        <Text className='font-medium'>绑定信息门户</Text>
                     </View>
                     <Form onSubmit={handleLogin}>
                         <View className='mt-8 flex flex-col justify-center'>
@@ -46,13 +51,13 @@ export default function Login({setIsLogged}) {
                                 <Input className='input' name='password' password placeholder='请输入密码'></Input>
                             </View>
                         </View>
-                        <Button className='text-gray-500 text-sm px-4 py-2 w-full mt-4 rounded-full bg-blue-300'
-                            formType='submit'
+                        <Button className='text-white text-sm px-4 py-2 w-full mt-4 rounded-full' style={{backgroundColor: "#359089"}}
+                          formType='submit'
                         >登录</Button>
                     </Form>
                 </View>
 
-                <Text className='text-blue-400 text-center text-sm mb-8'>遇到问题</Text>
+                <Text className='text-center text-sm font-medium mb-8' style={{color: "#385FB0"}}>遇到问题</Text>
             </View>
         </View>
     )
