9fbc3f44b8a878345d35139cca5e58e67dd2d1ce
fzxiao233 <fzxiao@outlook.com>
2021-10-29 22:29:18 +0800

    feat: assignment

--- src/pages/index/components/assignment_status.tsx
+++ src/pages/index/components/assignment_status.tsx
@@ -8,8 +8,8 @@ export function AssignmentStatus() {
     const [show, setShow] = useState(false)
     const updateStatus = () => {
         getCurrAssignmentStatus().then((res) => {
-            if (res.data.code === 0 && res.data.data.length > 0) {
-                setRecentStatus(res.data.data[0])
+            if (res.data.code === 0 && res.data.data !== undefined) {
+                setRecentStatus(res.data.data)
                 setShow(true)
             } else {
                 Taro.showToast({
@@ -44,22 +44,22 @@ export function AssignmentStatus() {
         })
     }
     return (show ?
-        <View className='card flex flex-col p-4 bg-black mt-2 text-white'>
+        <View className='card flex flex-col p-4 mt-2'>
             <View className='flex'>
                 <Text className='text-sm'>预约ID：{currStatus.appointmentId}</Text>
             </View>
             <View className='flex justify-between'>
                 <View className='flex flex-col'>
-                    <Text>{currStatus.buildId}</Text>
+                    <Text>{currStatus.buildId}号楼</Text>
                     <Text className='text-xs'>{currStatus.seatId} 号</Text>
                 </View>
                 <View className='self-end'>
-                    <Text className='mr-2'>{currStatus.startTime} - {currStatus.endTime}</Text>
+                    <Text className='mr-2 text-sm'>{currStatus.startTime} - {currStatus.endTime}</Text>
                 </View>
             </View>
             <View className='flex justify-between mt-2'>
-                {currStatus.state === 0 ? <Button size='mini' onClick={() => handleSeat(2)}>暂离座位</Button> : <Button size='mini' onClick={() => handleSeat(1)}>回到座位</Button>}
-                <Button size='mini'>结束学习</Button>
+                {currStatus.state === 0 ? <Button size='mini' className='button' onClick={() => handleSeat(2)}>暂离座位</Button> : <Button size='mini' className='button' onClick={() => handleSeat(1)}>回到座位</Button>}
+                <Button size='mini' className='button'>结束学习</Button>
             </View>
         </View> : <></>
     )
